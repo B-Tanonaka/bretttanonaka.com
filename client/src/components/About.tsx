9cb177f8aad9f6b49b69661f189dcfa3de5c3b78
Brett Tanonaka <tano.brett@gmail.com>
2023-07-08 16:19:55 -0700

    About page text-aligned right and spacing between hr set

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import data from '../projectData';
 
 export default function About() {
   return (
     <div className="about">
-      <span>
+      <div className="about-wrapper">
         <h1>About</h1>
-      </span>
+        <hr style={{
+          border: '0.03em dashed black',
+          boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
+        }}
+        />
+        <span>
+          <p>{data.description}</p>
+        </span>
+      </div>
     </div>
   );
 }
