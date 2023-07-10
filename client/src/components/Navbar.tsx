8e122c4474d6deeb73d3bd72cd1c94220b1e779f
Brett Tanonaka <tano.brett@gmail.com>
2023-07-10 15:57:31 -0700

    Behind nav has blur filter

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -12,16 +12,18 @@ export default function Navbar() {
   // };
 
   return (
-    <div className="navbar-container">
-      <a href="/" className="navbar-name">Brett Tanonaka</a>
-      <nav className="navbar-base">
-        <ul className="navbar-sections">
-          <li><a href="/">Home</a></li>
-          <li><a href="/projects">Projects</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
+    <div className="navbar-border">
+      <div className="navbar-container">
+        <a href="/" className="navbar-name">Brett Tanonaka</a>
+        <nav className="navbar-base">
+          <ul className="navbar-sections">
+            <li><a href="/">Home</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
