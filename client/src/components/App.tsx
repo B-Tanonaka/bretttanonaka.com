516bba36812fb329b6afefa3e10174ff80569e1c
Brett Tanonaka <tano.brett@gmail.com>
2023-07-10 10:46:49 -0700

    Added max-width/height 100% when using vh/vw to get rid of extra scroll bar space

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,11 +3,13 @@ import Home from './Home';
 import Navbar from './Navbar';
 import Projects from './Projects';
 import About from './About';
+import SingleProject from './SingleProject';
 
 export default function App() {
   return (
     <div className="app">
       <Navbar />
+      <SingleProject />
       <Home />
       <Projects />
       <About />
--- client/src/components/SingleProject.tsx
+++ client/src/components/SingleProject.tsx
@@ -5,24 +5,22 @@ import data from '../projectData';
 export default function SingleProject() {
   return (
     <div className="single-project">
-      <span>
-        <p>{data.projectData[0].description}</p>
-      </span>
+      <p>{data.projectData[0].description}</p>
       <div className="right-side">
-        <h1>{data.projectData[0].name}</h1>
+        <h2>{data.projectData[0].name}</h2>
         <p>{data.projectData[0].year}</p>
         <hr style={{
           border: '0.03em dashed black',
           boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
         }}
         />
-        { data.projectData[0].role.map((role: string) => (<p>{role}</p>))}
+        { data.projectData[0].role.map((role: string) => (<div>{role}</div>))}
         <hr style={{
           border: '0.03em dashed black',
           boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
         }}
         />
-        { data.projectData[0].techStack.map((framework: string) => (<p>{framework}</p>))}
+        { data.projectData[0].techStack.map((framework: string) => (<div>{framework}</div>))}
       </div>
     </div>
   );
