572be45e59a224affccee2e09883d672f01317ad
Brett Tanonaka <tano.brett@gmail.com>
2023-07-11 10:26:53 -0700

    Changed name/email in form to labels

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -16,26 +16,36 @@ export default function Contact() {
 
   return (
     <div className="contact">
-      <h1>Contact</h1>
-      <hr style={{
-        border: '0.03em dashed black',
-        boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
-      }}
-      />
-      <form className="form">
-        <div className="form-name">
-          Name
-          <input type="text" name="name" value={form.name} onChange={handleChange} />
-        </div>
-        <div className="form-email">
-          Email
-          <input type="text" name="email" value={form.email} onChange={handleChange} />
-        </div>
-        <div className="form-message">
-          Message
-          <input type="text" name="message" value={form.message} onChange={handleChange} />
-        </div>
-      </form>
+      <div className="contact-container">
+        <h1>Contact</h1>
+        <hr style={{
+          border: '0.03em dashed black',
+          boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
+          marginBottom: '5%',
+        }}
+        />
+        <form className="form">
+          <div className="top-row">
+            <div className="name-email">
+              <label htmlFor="input-name">
+                Name
+                <input type="text" name="name" value={form.name} onChange={handleChange} className="input-box-top" />
+              </label>
+            </div>
+            <div className="name-email">
+              <label htmlFor="input-email">
+                Email
+                <input type="text" name="email" value={form.email} onChange={handleChange} className="input-box-top" />
+              </label>
+            </div>
+          </div>
+          <div className="form-message">
+            <label className="form-title">Message</label>
+            {/* <input type="textarea" name="message" value={form.message} onChange={handleChange} className="input-box-bottom" /> */}
+          </div>
+          <input type="button" name="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
