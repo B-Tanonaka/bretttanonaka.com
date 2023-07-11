26d543222b3aa3d3696ba0598612c26a75d3f625
Brett Tanonaka <tano.brett@gmail.com>
2023-07-11 12:35:38 -0700

    Install types for react-scroll

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,16 +19,16 @@ export default function Navbar() {
         <nav className="navbar-base">
           <ul className="navbar-sections">
             <li>
-              <Link to="home" smooth offset={50} duration={500}>Home</Link>
+              <Link to="home" smooth spy offset={50} duration={400}>Home</Link>
             </li>
             <li>
-              <Link to="projects" smooth offset={50} duration={500}>Projects</Link>
+              <Link to="projects" smooth spy offset={50} duration={400}>Projects</Link>
             </li>
             <li>
-              <Link to="about" smooth offset={50} duration={500}>About</Link>
+              <Link to="about" smooth spy offset={50} duration={400}>About</Link>
             </li>
             <li>
-              <Link to="contact" smooth offset={50} duration={500}>Contact</Link>
+              <Link to="contact" smooth spy offset={50} duration={400}>Contact</Link>
             </li>
           </ul>
         </nav>
