6720441fb87075d7f85256aeb84bff525ff12226
Brett Tanonaka <tano.brett@gmail.com>
2023-07-10 17:06:20 -0700

    Form name, email, message sections added

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -16,8 +16,25 @@ export default function Contact() {
 
   return (
     <div className="contact">
-      <form>
-
+      <h1>Contact</h1>
+      <hr style={{
+        border: '0.03em dashed black',
+        boxShadow: '5px 2px 5px 0.5px hsl(0deg 0% 0% / 22%)',
+      }}
+      />
+      <form className="form">
+        <div className="form-name">
+          Name
+          <input type="text" name="name" value={form.name} onChange={handleChange} />
+        </div>
+        <div className="form-email">
+          Email
+          <input type="text" name="email" value={form.email} onChange={handleChange} />
+        </div>
+        <div className="form-message">
+          Message
+          <input type="text" name="message" value={form.message} onChange={handleChange} />
+        </div>
       </form>
     </div>
   );
