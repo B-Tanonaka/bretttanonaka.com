4163a57f25e72171d0435d49877a31b2073d664a
Brett Tanonaka <tano.brett@gmail.com>
2023-07-13 10:07:16 -0700

    Created React Router in main index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
