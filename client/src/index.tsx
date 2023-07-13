a2e4bafd4bef3730b646ff2d3b2d7a7e5b8ff69f
Brett Tanonaka <tano.brett@gmail.com>
2023-07-13 14:41:54 -0700

    Switching from createBrowserRouter to BrowserRouter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
+import Error from './components/Error';
+import ProjectDetails from './components/ProjectDetails';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/projects/:projectId',
+    element: <ProjectDetails />,
   },
 ]);
 
