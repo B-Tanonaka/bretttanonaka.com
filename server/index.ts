969b281447b5748c868620c810b5fa6db915ab98
Brett Tanonaka <tano.brett@gmail.com>
2024-02-08 15:19:44 -0800

    Installed airbnb-typescript linter

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,7 @@
 import morgan from 'morgan';
 import express from 'express';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import cors from 'cors';
 import router from './router';
-import dotenv from 'dotenv';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3131;
@@ -17,8 +10,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, '../portfolio')));
 
 app.use('/', router);
 
