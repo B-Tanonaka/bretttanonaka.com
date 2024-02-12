4633e358d35473dbc7ae55264c162b1bb9573932
Brett Tanonaka <tano.brett@gmail.com>
2024-02-12 15:00:26 -0800

    Error moved to own CSS file; added background styling

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,25 +1,15 @@
-// import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import '../css/Error.css';
 
 export default function Error() {
-  // const error = useRouteError();
-  // let errorMessage: string;
-
-  // if (isRouteErrorResponse(error)) {
-  //   errorMessage = error.statusText;
-  // } else if (typeof error === 'string') {
-  //   errorMessage = error;
-  // } else {
-  //   errorMessage = 'Unknown error';
-  // }
-
   return (
     <div id="error" className="error-container">
       <h1>404</h1>
-      <p>An an unexpected error has occurred</p>
+      <p>Sorry! Something went wrong.</p>
       <Link to="/">
         <button type="button" className="error-home-button">Home</button>
       </Link>
+      <div className="error-background" />
     </div>
   );
 }
