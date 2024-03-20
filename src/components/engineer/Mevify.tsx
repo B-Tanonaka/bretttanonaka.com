d3da09b323b98ab5e92b079c7d7dfc761ef755d6
Brett Tanonaka <tano.brett@gmail.com>
2024-03-20 15:54:34 -0700

    Mevify formatted for all devices; black background changed to eng-graident

--- src/components/engineer/Mevify.tsx
+++ src/components/engineer/Mevify.tsx
@@ -6,9 +6,9 @@ export default function Mevify(
   { projectData: EngineerProject | null, renderProjectImages: RenderPhotoList },
 ) {
   // Scroll to top of page upon load
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="details-container">
