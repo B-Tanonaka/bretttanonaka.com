bf8c35bbcee2e123dbc024e52d3cf026c176cea3
Brett Tanonaka <tano.brett@gmail.com>
2024-03-20 14:04:31 -0700

    Portfolio formatted for all devices

--- src/components/engineer/Portfolio.tsx
+++ src/components/engineer/Portfolio.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Carousel from '../Carousel';
 import type { EngineerProject, RenderPhotoList } from '../../../interfaces';
 
 export default function Portfolio(
@@ -12,11 +13,55 @@ export default function Portfolio(
 
   return (
     <div className="details-container">
+      <div className="details-container-h3-solo">
+        <h3>{projectData!.text.goal.part1}</h3>
+      </div>
+      { /* Design in Fimga */ }
       <h1>Original Design in Figma</h1>
-      <div className="image-wrapper">
-        { projectData!.images.figma.map(
-          (img: { src: string, alt: string }, key: number) => renderProjectImages(img, key, 'desktop-image'),
-        ) }
+      <div className="carousel-wrapper carousel-li-companies">
+        <Carousel
+          images={projectData!.images.figma}
+          type="single-landscape"
+        />
+      </div>
+      { /* Background */ }
+      <h1>Background</h1>
+      <div className="image-2-container">
+        <h5 className="image-2-col">
+          <span>{projectData!.text.background.part1}</span>
+          <br />
+          <br />
+          <span>{projectData!.text.background.part2}</span>
+        </h5>
+        <img
+          className="image-2-col"
+          src={projectData!.images.gifs.background.src}
+          alt={projectData!.images.gifs.background.alt}
+        />
+      </div>
+      { /* Project Details */ }
+      <h1>Project Details</h1>
+      <div className="image-2-container">
+        <img
+          className="image-2-col"
+          src={projectData!.images.gifs.projectDetails.src}
+          alt={projectData!.images.gifs.projectDetails.alt}
+        />
+        <h5 className="image-2-col">
+          <span>{projectData!.text.projectDetails.part1}</span>
+        </h5>
+      </div>
+      { /* Contact */ }
+      <h1>Contact</h1>
+      <div className="image-2-container">
+        <h5 className="image-2-col">
+          <span>{projectData!.text.contact.part1}</span>
+        </h5>
+        <img
+          className="image-2-col"
+          src={projectData!.images.gifs.contact.src}
+          alt={projectData!.images.gifs.contact.alt}
+        />
       </div>
     </div>
   );
