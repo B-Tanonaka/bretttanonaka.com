a7248e8ff4ebd9e91f42baa945d6c2177a7f2aea
Brett Tanonaka <tano.brett@gmail.com>
2026-05-08 15:37:02 -0700

    Added the videos and descriptions for Google Antigravity page

--- src/components/video/GoogleDeepmind.tsx
+++ src/components/video/GoogleDeepmind.tsx
@@ -22,6 +22,34 @@ export default function GoogleDeepmind(
           className="single-video"
         />
       </div>
+      {/* What brings you joy images */}
+      <div className="image-2-container">
+        <img
+          className="image-2-col-wide"
+          src={projectData!.images.interface.src}
+          alt={projectData!.images.interface.alt}
+        />
+        <div className="image-2-col-text-wrapper">
+          <img
+            className="image-2-col-text"
+            src={projectData!.images.speaker.src}
+            alt={projectData!.images.speaker.alt}
+          />
+          <h5 className="recruiting-quote-text">{projectData!.images.interface.desc}</h5>
+        </div>
+      </div>
+      {/* Agent Skills */}
+      <h1>Agent Skills</h1>
+      <div className="video-2-container motion-graphics-million-container">
+        <h5>
+          {projectData!.videos.skills.desc}
+        </h5>
+        <VideoWithBlurb
+          info={projectData!.videos.skills}
+          contClassName="video-2-col video-2-col-right"
+          className="single-video"
+        />
+      </div>
     </div>
   );
 }
